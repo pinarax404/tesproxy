8e8bd625a2c665b91ba5539bae77d4c666951b73
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2022-05-13 13:31:57 +0700

    Update tes.php

--- tes.php
+++ tes.php
@@ -1,14 +1,17 @@
 <?php
 
-$opts = array(
-    'http'=>array(
-        'method' => "GET",
-        'socks5' => '5.9.2.236:1080',
-    )
-);
+$url = 'https://ipwhois.app/json/';
+$proxy = '5.9.2.236:1080';
+//$proxyauth = 'user:password';
 
-$context = stream_context_create($opts);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+//curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HEADER, 1);
+$curl_scraped_page = curl_exec($ch);
+curl_close($ch);
 
-$file = file_get_contents('https://ipwhois.app/json/', false, $context);
-
-echo $file;
+echo $curl_scraped_page;
