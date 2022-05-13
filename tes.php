43d3b2c01385aba6bb4d557bc84cba61bd79dd9b
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2022-05-13 12:20:00 +0700

    Update tes.php

--- tes.php
+++ tes.php
@@ -3,16 +3,16 @@
 aaa();
 
 function aaa() {
-    $c = curl_init();
-    curl_setopt($c, CURLOPT_URL, 'https://ipwhois.app/json/');
-    curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($c, CURLOPT_SSL_VERIFYHOST, false);
-    curl_setopt($c, CURLOPT_HTTPPROXYTUNNEL, true);
-    curl_setopt($c, CURLOPT_PROXY, 'us-central-062.whiskergalaxy.com:443');
+    $opts = array('http' =>
+        array(
+            'proxy'  => 'https://us-central-062.whiskergalaxy.com:443',
+        )
+    );
 
-    $ret = curl_exec($c);
-    if(strpos($ret, 'ip') !== false) {
+    $context  = stream_context_create($opts);
+    $result = file_get_contents('https://ipwhois.app/json/', false, $context);
+
+    if(strpos($result, 'ip') !== false) {
         $res_get_ip = json_decode($ret, true);
         echo "\033[1;37mIP : " . $res_get_ip['ip'] . " | Country : " . $res_get_ip['country'] . "\033[1;37m\n";
         aaa();
