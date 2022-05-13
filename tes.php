b4545c2ea5a7331df1ec2c30a37545a72d67111f
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2022-05-13 13:35:07 +0700

    Update tes.php

--- tes.php
+++ tes.php
@@ -1,17 +1,25 @@
 <?php
 
-$url = 'https://ipwhois.app/json/';
-$proxy = 'socks5://5.9.2.236:1080';
-//$proxyauth = 'user:password';
+aaa();
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_PROXY, $proxy);
-//curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_HEADER, 1);
-$curl_scraped_page = curl_exec($ch);
-curl_close($ch);
+function aaa() {
+    $proxy = 'https://wf-us-015.whiskergalaxy.com:443';
 
-echo $curl_scraped_page;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://ipwhois.app/json/');
+    curl_setopt($ch, CURLOPT_PROXY, $proxy);
+    curl_setopt($ch, HTTP_PROXY_REQUEST_FULLURI, false);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $curl_scraped_page = curl_exec($ch);
+    curl_close($ch);
+
+    if(strpos($curl_scraped_page, 'ip') !== false) {
+        $res_get_ip = json_decode($curl_scraped_page, true);
+        echo "\033[1;37mIP : " . $res_get_ip['ip'] . " | Country : " . $res_get_ip['country'] . "\033[1;37m\n";
+        aaa();
+    } else {
+        echo "\033[1;37mIP : null | Country : null\033[1;37m\n";
+        aaa();
+    }
+}
