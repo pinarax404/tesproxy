f440a6a7e47cefb0e914ef2192cd2637c3a0ec0f
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2022-05-13 12:50:52 +0700

    Update tes.php

--- tes.php
+++ tes.php
@@ -1,14 +1,12 @@
 <?php
 
-$sURL = "http://beamtic.com/Examples/ip.php"; // The Request URL
+$context = stream_context_create([
+    'http' => [
+        'proxy' => 'wf-us-015.whiskergalaxy.com:443',
+        'request_fulluri' => true
+    ]
+]);
 
-$aHTTP['http']['proxy']           = 'wf-us-015.whiskergalaxy.com:443'; // The proxy ip and port number
-$aHTTP['http']['request_fulluri'] = true; // use the full URI in the Request. I.e. http://beamtic.com/Examples/ip.php
-$aHTTP['http']['method']          = 'GET';
-$aHTTP['http']['header']          = "User-Agent: My PHP Script\r\n";
-$aHTTP['http']['header']         .= "Referer: http://beamtic.com/\r\n";
+$result = file_get_contents('https://ipwhois.app/json/', false, $context);
 
-$context = stream_context_create($aHTTP);
-$contents = file_get_contents($sURL, false, $context);
-
-echo 'Proxy IP:'. $contents;
+echo $result;
