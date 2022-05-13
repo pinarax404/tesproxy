bebf50e5b0a6a2f5643224903fc7b98384f2ab7c
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2022-05-13 13:16:21 +0700

    Update tes.php

--- tes.php
+++ tes.php
@@ -1,13 +1,5 @@
 <?php
 
-$options = array(
-    'http' => array(
-		'proxy' => 'tcp://5.9.2.236:1080',
-		'request_fulluri' => true
-    )
-);
-
-$context = stream_context_create($options);
-$result = file_get_contents('http://ip-api.com/json/', false, $context);
+$result = file_get_contents('http://ip-api.com/json/', false);
 
 echo $result;
