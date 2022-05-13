10b66f47a7340c2d219d871eedfae88c921c4272
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2022-05-13 12:45:55 +0700

    Update tes.php

--- tes.php
+++ tes.php
@@ -1,23 +1,14 @@
 <?php
 
-aaa();
+$sURL = "http://beamtic.com/Examples/ip.php"; // The Request URL
 
-function aaa() {
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://ipwhois.app/json/');
-    curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_HTTPS);
-    curl_setopt($ch, CURLOPT_PROXY, 'wf-us-015.whiskergalaxy.com:443');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // read more about HTTPS http://stackoverflow.com/questions/31162706/how-to-scrape-a-ssl-or-https-url/31164409#31164409
-    curl_setopt($ch, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
-    $output = curl_exec($ch);
-    curl_close($ch);
-    if(strpos($output, 'ip') !== false) {
-        $res_get_ip = json_decode($output, true);
-        echo "\033[1;37mIP : " . $res_get_ip['ip'] . " | Country : " . $res_get_ip['country'] . "\033[1;37m\n";
-        aaa();
-    } else {
-        echo "\033[1;37mIP : null | Country : null\033[1;37m\n";
-        aaa();
-    }
-}
+$aHTTP['http']['proxy']           = 'tcp://wf-us-015.whiskergalaxy.com:443'; // The proxy ip and port number
+$aHTTP['http']['request_fulluri'] = true; // use the full URI in the Request. I.e. http://beamtic.com/Examples/ip.php
+$aHTTP['http']['method']          = 'GET';
+$aHTTP['http']['header']          = "User-Agent: My PHP Script\r\n";
+$aHTTP['http']['header']         .= "Referer: http://beamtic.com/\r\n";
+
+$context = stream_context_create($aHTTP);
+$contents = file_get_contents($sURL, false, $context);
+
+echo 'Proxy IP:'. $contents;
