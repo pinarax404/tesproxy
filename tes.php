351ceac42fc436f2b7790d05d8d55e2f2da34508
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2022-05-13 12:14:56 +0700

    Update tes.php

--- tes.php
+++ tes.php
@@ -1,21 +1,22 @@
 <?php
 
-$opts = array(
-    'ssl' => array(
-        'verify_peer' => false,
-        'verify_peername' => false
-    ),
-    'http' => array(
-        'proxy' => 'us-central-062.whiskergalaxy.com:443',
-        'request_fulluri'=>true,
-        'header' => array(
-            "Accept-language: en"
-            "Content-type: application/x-www-form-urlencoded"
-         )
-    )
-);
+aaa();
 
-$context = stream_context_create($opts);
-$s = file_get_contents("https://ipwhois.app/json/", false, $context);
+function aaa() {
+    $ret = curl_setopt($c, CURLOPT_URL, 'https://ipwhois.app/json/');
+    $ret = curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+    $ret = curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
+    $ret = curl_setopt($c, CURLOPT_SSL_VERIFYHOST, false);
+    $ret = curl_setopt($c, CURLOPT_HTTPPROXYTUNNEL, true);
+    $ret = curl_setopt($c, CURLOPT_PROXY, 'us-central-062.whiskergalaxy.com:443');
 
-echo $s;
+    $ret = curl_exec($c);
+    if(strpos($ret, 'ip') !== false) {
+        $res_get_ip = json_decode($ret, true);
+        echo "\033[1;37mIP : " . $res_get_ip['ip'] . " | Country : " . $res_get_ip['country'] . "\033[1;37m\n";
+        aaa();
+    } else {
+        echo "\033[1;37mIP : null | Country : null\033[1;37m\n";
+        aaa();
+    }
+}
