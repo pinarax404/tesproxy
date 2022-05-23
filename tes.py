4b0ae09a5e3bb7dfa379049f49e80cbd099f37ad
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2022-05-23 20:28:51 +0700

    Create tes.py

---
+++ tes.py
@@ -0,0 +1,9 @@
+import requesocks
+import requests
+
+s = requesocks.session()
+s.proxies = {'http':  'socks5://127.0.0.1:9050',
+            'https': 'socks5://127.0.0.1:9050'}
+
+s.get('https://ipwhois.app/json/').json()
+print(s)
