c6377ce2f3883c7b877e4c11872f76771fcf21b1
pinarax404 <92956633+pinarax404@users.noreply.github.com>
2022-05-23 20:37:05 +0700

    Update tes.py

--- tes.py
+++ tes.py
@@ -6,7 +6,7 @@ session.proxies = {
     'http': 'socks5://127.0.0.1:9050',
     'https': 'socks5://127.0.0.1:9050'
 }
-r = session.get('https://ipwhois.app/json/')
+r = session.get('https://www.instagram.com')
 print(r.status_code)
 print(r.headers['content-type'])
 print(r.text)
